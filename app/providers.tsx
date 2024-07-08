70acab681a084d56ec14752c315de63e3ccbb677
shoukathmd <shoukathmd@gmail.com>
2024-07-08 19:07:47 +0530

    Added skeleton

--- app/providers.tsx
+++ app/providers.tsx
@@ -1,5 +1,5 @@
 "use client";
-import { ThemeProvider } from "@/components/Theme-provider";
+import { ThemeProvider } from "../components/Theme-provider";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import { useState } from "react";
