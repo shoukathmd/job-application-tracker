b71272b8251ac4aa4feea1fe86a0ce906bb05489
shoukathmd <shoukathmd@gmail.com>
2024-07-08 19:06:51 +0530

    Added skeleton

--- app/providers.tsx
+++ app/providers.tsx
@@ -4,6 +4,7 @@ import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import { useState } from "react";
 import { Toaster } from "@/components/ui/toaster";
+
 const Providers = ({ children }: { children: React.ReactNode }) => {
   const [queryClient] = useState(
     () =>
@@ -17,6 +18,7 @@ const Providers = ({ children }: { children: React.ReactNode }) => {
         },
       })
   );
+
   return (
     <>
       <ThemeProvider
@@ -34,4 +36,5 @@ const Providers = ({ children }: { children: React.ReactNode }) => {
     </>
   );
 };
+
 export default Providers;
